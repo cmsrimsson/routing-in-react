f76fc62233b3a1aca8e83d194fd2df0c8327d61a
cmsrimsson <cmsrimsson@gmail.com>
2021-02-02 21:06:26 -0500

    1st commit

---
+++ src/component/404/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function NotFound() {
+    return (
+        <div className="NotFound">
+            404, Page Not Found !
+        </div>
+    )
+}
+
+export default NotFound;
